1693544b4d2304105e4e180e9f6e8ac34dda9a71
Alexandre-ro <alexandre.programador84@gmail.com>
2021-11-28 22:04:45 -0300

    Feito integração com a API para Login e Cadastro de Livros

--- src/pages/Book/NewBook.js
+++ src/pages/Book/NewBook.js
@@ -1,9 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import './styleNewBook.css';
+import api from "../../services/Api";
 
 function NewBook() {
+  const[title, setTitle]            = useState('');
+  const[author,setAuthor]           = useState('');
+  const[launchDate, setlaunchDate]  = useState('');
+  const[price, setPrice]            = useState('');
+  const history                     = useHistory();
+
+  async function Adicionar(e){
+    e.preventDefault();
+
+    const data = {
+        title,
+        author,
+        launchDate,
+        price
+    };
+
+    const accessToken = localStorage.getItem('accessToken');
+   
+    try{
+        await api.post('api/Book/v1', data, {
+          headers:{
+            Authorization: `Bearer ${accessToken}`
+          }
+        });
+    }catch(err){
+        alert('Ocorreu um erro ao salvar o livro!');
+    }
+
+    history.push('/book');
+  }
+  
   return (
     <div className="new-book">
       <div className="content">
@@ -15,17 +47,17 @@ function NewBook() {
             Home
           </Link>
         </section>
-        <form>
-          <input placeholder="Titulo" />
-          <input placeholder="Autor" />
-          <input type="date" />
-          <input placeholder="Preço" />
+        <form onSubmit={Adicionar}>
+          <input placeholder="Titulo" value={title} onChange= {e => setTitle(e.target.value)} />
+          <input placeholder="Autor" value={author} onChange= {e => setAuthor(e.target.value)} />
+          <input type="date" value={launchDate} onChange = {e => setlaunchDate(e.target.value)} />
+          <input placeholder="Preco" value={price} onChange = {e => setPrice(e.target.value)} />
           <button className="button" type="submit">
             Adicionar
           </button>
         </form>
       </div>
-    </div>
+    </div> 
   );
 }
 
--- src/pages/Login/Index.js
+++ src/pages/Login/Index.js
@@ -1,15 +1,46 @@
-import React from "react";
+import {React, useState} from "react";
+import { useHistory } from "react-router-dom";
 import "./styles.css";
 import padlock from "../../assets/padlock.png";
+import api from "../../services/Api";
 
 function Login() {
+
+const [userName, setUserName] = useState('');
+const [password, setPassword] = useState('');
+const history               = useHistory();
+
+async function logar(e){
+  e.preventDefault();
+
+  const data = {
+    userName,
+    password
+  };
+
+    try{
+      const response = await api.post('api/auth/v1/signin', data);
+      console.log('dsd'+response);
+
+      localStorage.setItem('userName', userName);
+      localStorage.setItem('accessToken', response.data.accessToken);
+      localStorage.setItem('refreshToken', response.data.refreshToken);
+      
+      //Em caso de sucesso redireciona para a página de livros
+      history.push('/book');
+
+    }catch(error){  
+      alert('A tentativa de logar falhou, tente novamente!');
+    }
+};
+
   return (
     <div className="login-container">
       <section className="form">
-        <form>
+        <form onSubmit={logar}>
           <h1>Acesse sua conta</h1>
-          <input type="text" placeholder="Usuário" />
-          <input type="password" placeholder="Senha" />
+          <input type="text" placeholder="Usuário" value={userName} onChange={e => setUserName(e.target.value)} />
+          <input type="password" placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)} />
           <button className="button" type="submit">
             Logar
           </button>
